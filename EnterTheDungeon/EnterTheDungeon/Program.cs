d1317122195ff476ff1cd0158871aab3d3ace311
Park-Junkyu <141114860+Park-Junkyu@users.noreply.github.com>
2023-08-22 11:45:33 +0900

    inven fixed

--- EnterTheDungeon/EnterTheDungeon/Program.cs
+++ EnterTheDungeon/EnterTheDungeon/Program.cs
@@ -2,12 +2,10 @@
 {
     public class Lobby // 게임 시작화면
     {
+        public int input;
         public void mainLobby()
         {
             Console.Clear();
-
-            MyChar character = new MyChar("Park", "Warrior", 1, 10, 5, 100, 1500);
-            Inventory inven = new Inventory();
 
             Console.WriteLine("엔터 더 던전에 오신 것을 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
@@ -16,38 +14,10 @@
             Console.WriteLine("2. 인벤토리 보기");
             Console.WriteLine(" ");
             Console.WriteLine("원하시는 행동을 입력해주세요.");
+            input = int.Parse(Console.ReadLine());
 
-            string input = Console.ReadLine();
-            if(input == "1")
-            {
-                character.ShowMyChar();
-                if(character.menu == 0)
-                {
-                    mainLobby();
-                }
-            }
-            else if(input == "2")
-            {
-                inven.ShowInven();
-                if (inven.menu == 0)
-                {
-                    mainLobby();
-                }else if(inven.menu == 1)
-                {
-                    inven.Equip();
-                    if (inven.menu == 0)
-                    {
-                        mainLobby();
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("잘못된 선택입니다.");
-                Thread.Sleep(1000);
-                mainLobby();
-            }
         }
+          
         // 1. 상태 보기로 전환
         // 2. 인벤토리 보기로 전환
     }
@@ -57,9 +27,9 @@
         public string Name { get; }
         public string Job { get; }
         public int Lv { get; }
-        public int Attack { get; }
-        public int Defend { get; }
-        public int Health { get; }
+        public int Attack { get; set; }
+        public int Defend { get; set; }
+        public int Health { get; set; }
         public int Gold { get; }
         public int menu;
         public MyChar( string _name, string _job, int _Lv, int _attack, 
@@ -97,35 +67,68 @@
             Console.WriteLine("원하시는 행동을 입력하시오");
             menu = int.Parse(Console.ReadLine() );
         }
+        
+        item item = new item();
+        public void Status(bool equip, int WeaponNum)
+        {
+            if (equip)
+            {
+                Attack += item.WeaponDmg[WeaponNum];
+            }
+        }
         // 0. 나가기(로비로)
     }
 
+    public class item
+    {
+        public string Name { get; set; }
+        public int Effect { get; set; }
+        public string script { get; set; }
+
+        public string[] WeaponName = { "Axe", "Bow" };
+        public int[] WeaponDmg = { 5, 10 };
+        public string[] WeaponScrpt = { "Basic Axe", "BasicBow" };
+
+        public void EquipItem(bool equip, int WeaponNum)
+        {
+            if (equip)
+            {
+                // 장착했을 경우 [E]을 WeaponName에 추가
+                WeaponName[WeaponNum] = "[E]" + WeaponName[WeaponNum];
+                
+            }
+        }
+
+        
+
+        public void UnequipItem(bool unequip, int WeaponNum)
+        {
+            if (unequip)
+            {
+                string substring = "]";
+                int firstsubstring = WeaponName[WeaponNum].IndexOf(substring);
+                WeaponName[WeaponNum] = WeaponName[WeaponNum].Remove(firstsubstring);
+            }
+        }
+    }
     public class Inventory // 인벤토리 보기
     {
+        item item = new item();
         public List<string> Inven;
         public int menu;
         public int select;
-        public void manageInven()
-        {
-            Inven = new List<string>();
-            string[] weapon = { "Axe", "Attack : 5", "Basic axe",
-                                "Bow", "Attack : 10", "Basic bow"};
-            for(int i = 0;i < weapon.Length; i++)
-            {
-                Inven.Add(weapon[i]);
-            }
-        }
+        public bool equip;
+        
 
         public void ShowInven()
         {
             Console.Clear();
-            manageInven();
 
             Console.WriteLine("[아이템 목록]");
             Console.WriteLine("[아이템 이름]   |  [장비 효과]  |  [장비 설명]  ");
-            for (int i =0;i<Inven.Count ;i+=3)
+            for (int i =0;i<2 ;i++)
             {
-                Console.WriteLine("\t" + Inven[i]+ "\t" + Inven[i+1] + "\t" + Inven[i+2] + "\n");
+                Console.WriteLine(item.WeaponName[i] +"\t|\t"+ item.WeaponDmg[i] +"\t|\t"+ item.WeaponScrpt[i]);
             }
             Console.WriteLine(" ");
             Console.WriteLine("1. 장착 관리");
@@ -135,44 +138,28 @@
             menu = int.Parse(Console.ReadLine());
         }
 
-        public void Equip()
+        
+        public void Equip(MyChar targetCharacter)
         {
             Console.Clear();
-            manageInven();
-
             Console.WriteLine("[아이템 목록]");
             Console.WriteLine("[아이템 이름]   |  [장비 효과]  |  [장비 설명]  ");
-            for (int i = 0; i < Inven.Count; i += 3)
+            for (int i = 0; i < item.WeaponName.Length; i++)
             {
-                Console.WriteLine("["+(i/3+1)+"]"+ "\t" + Inven[i] + "\t" + Inven[i + 1] + "\t" + Inven[i + 2] + "\n");
+                Console.WriteLine("["+(i+1)+"]" + item.WeaponName[i] + "\t|\t" + item.WeaponDmg[i] + "\t|\t" + item.WeaponScrpt[i]);
             }
             Console.WriteLine("장착할 장비를 선택하시오");
             select = int.Parse(Console.ReadLine());
             if(select != null)
             {
-                Console.Clear();
-
-                Console.WriteLine("[아이템 목록]");
-                Console.WriteLine("[아이템 이름]   |  [장비 효과]  |  [장비 설명]  ");
-                for (int i = 0; i < Inven.Count; i += 3)
-                {
-                    // 무기 장착시 리스트에 이름을 [E]를 붙혀서 대체하는 방법은 어떨지?
-                    if (select == (i / 3 + 1))
-                    {
-                        Console.WriteLine("[E]" + "[" + (i / 3 + 1) + "]" + "\t" + Inven[i] + "\t" + Inven[i + 1] + "\t" + Inven[i + 2] + "\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("[" + (i / 3 + 1) + "]" + "\t" + Inven[i] + "\t" + Inven[i + 1] + "\t" + Inven[i + 2] + "\n");
-                    }
-                }
-                Console.WriteLine(" ");
-                Console.WriteLine("0. 나가기");
-                Console.WriteLine(" ");
-                Console.WriteLine("원하시는 행동을 입력하시오");
-                menu = int.Parse(Console.ReadLine());
+                equip = true;
             }
+            item.EquipItem(equip, select-1);
+            targetCharacter.Status(equip,select-1);
+            ShowInven();
+            
         }
+        
         // [아이템목록]
         // [아이템 이름]   |  장비 효과  |  장비 설명
         // 1. 장착 관리
@@ -181,10 +168,35 @@
 
     public class Program
     {
+        static MyChar character = new MyChar("Park", "Warrior", 1, 10, 5, 100, 1500);
+        static Inventory inven = new Inventory();
+        static Lobby lobby = new Lobby();
+        static item item = new item();
         static void Main(string[] args)
         {
-            Lobby lobby = new Lobby();
-            lobby.mainLobby();
+            
+            while (true)
+            {
+                lobby.mainLobby();
+                switch (lobby.input)
+                {
+                    case 1:
+                        character.ShowMyChar();
+                        break;                        
+                    case 2:
+                        inven.ShowInven();
+                        if (inven.menu == 1)
+                        {
+                            inven.Equip(character);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다.");
+                        Thread.Sleep(1000);
+                        break;
+                }
+            }          
+            
         }
     }
 
